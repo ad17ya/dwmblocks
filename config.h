aa4c346b9e2769692087014ff790ca8a06d57b77
Aditya Kshirsagar <aditya1751999@gmail.com>
2020-11-12 23:38:53 +0530

    modified for self

--- config.h
+++ config.h
@@ -3,20 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
-	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
+	{"",	"torrent",	20,	7},
 	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
 	{"",	"nettraf",	1,	16},
@@ -24,7 +12,21 @@ static const Block blocks[] = {
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+
+	/* {"",	"disk" ,	0,	10}, */
+	/*{"",	"news",		0,	6},*/
+	/*{"",	"memory",	10,	14}, */
+	/*{"",	"cpu",		10,	18}, */
+	/*{"",	"music",	0,	11}, */
+	/* {"",	"crypto",	0,	13}, */
+	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"moonphase",	18000,	17}, */
+	/*{"",	"help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
